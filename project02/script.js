3896ff8fd73f44f6e7e6debbca50985da34a6694
ZD-ong <384474574@qq.com>
2017-10-18 14:21:59 +0800

    Update script.js

--- project02/script.js
+++ project02/script.js
@@ -30,7 +30,7 @@ var top250 = {
         _this.isLoading = true;
         _this.$element.find('.loading').show();
         $.ajax({
-            url: 'http://api.douban.com/v2/movie/top250',
+            url: 'https://api.douban.com/v2/movie/top250',
             data: {
                 start: _this.index||0
             },
@@ -116,7 +116,7 @@ var usBox = {
         _this.isLoading = true;
         _this.$element.find('.loading').show();
         $.ajax({
-            url: 'http://api.douban.com/v2/movie/us_box',
+            url: 'https://api.douban.com/v2/movie/us_box',
             dataType: 'jsonp'
         }).done(function(ret){
             callback && callback(ret)
@@ -200,7 +200,7 @@ var search = {
         var _this = this;
         _this.$element.find('.loading').show();
         $.ajax({
-            url: 'http://api.douban.com/v2/movie/search',
+            url: 'https://api.douban.com/v2/movie/search',
             data: {
                 q: _this.keyword
             },
